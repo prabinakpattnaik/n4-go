ed8430e1f56b536f0800562ef3580084e13f467f
sothy5 <sothy.e98@gmail.com>
2019-06-15 07:08:53 +0200

    NodeID(IPv4) IE suppport added

--- ie/ie_test.go
+++ ie/ie_test.go
@@ -49,3 +49,23 @@ func TestNewInformationElement(t *testing.T) {
 	t.Log(i)
 
 }
+
+func TestIENodeID(t *testing.T) {
+
+	i := NewInformationElement(
+		IENodeID,                        //IEcode
+		0,                               //EntrepriseID
+		dt.OctetString("192.168.1.101"), //seconds in since January 1, 1900 UTC
+	)
+
+	if i.Length < 8 {
+		t.Fatalf("Unexpected length. want 8, have %d", i.Length)
+	}
+
+	ba := []byte{0x00, 0x3c, 0x00, 0x0d, 0x31, 0x39, 0x32, 0x2e, 0x31, 0x36, 0x38, 0x2e, 0x31, 0x2e, 0x31, 0x30, 0x31}
+
+	if b, _ := i.Serialize(); !bytes.Equal(b, ba) {
+		//t.Fatalf("[%x]", n.Serialize())
+		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, b)
+	}
+}
