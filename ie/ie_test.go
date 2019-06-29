12707464fde21b35076ffcbe324fca9888625b6f
sothy shan <sothy.e98@gmail.com>
2019-06-29 12:18:53 +0000

    Merged in UPFunctionFeatures (pull request #2)
    UPFunctionFeatures
    
    * NodeID(IPv4) IE suppport added
    
    * remove comments and add test support for up function features
    
    * correction to MR
    
    * add cp function features file
    
    * removed swp file

--- ie/ie_test.go
+++ ie/ie_test.go
@@ -42,10 +42,69 @@ func TestNewInformationElement(t *testing.T) {
 		t.Fatalf("Unexpected length in ii, want 8, have %d", ii.Length)
 	}
 	if bb, _ := ii.Serialize(); !bytes.Equal(bb, ba) {
-		//t.Fatalf("[%x]", n.Serialize())
 		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, bb)
 	}
 
 	t.Log(i)
+
+}
+
+func TestIENodeID(t *testing.T) {
+
+	nodeID := []byte{0x0, 0xC0, 0xa8, 0x1, 0x65}
+	i := NewInformationElement(
+		IENodeID, //IEcode
+		0,        //EntrepriseID
+		dt.OctetString(nodeID),
+	)
+
+	if i.Length != 9 {
+		t.Fatalf("Unexpected length. want 9, have %d", i.Length)
+	}
+
+	ba := []byte{0x00, 0x3c, 0x00, 0x9, 0x0, 0xC0, 0xa8, 0x1, 0x65}
+
+	if b, _ := i.Serialize(); !bytes.Equal(b, ba) {
+		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, b)
+	}
+}
+
+func TestIEUPFunctionFeatures(t *testing.T) {
+
+	i := NewInformationElement(
+		IEUPFunctionFeatures,               //IEcode
+		0,                                  //EntrepriseID
+		dt.OctetString([]byte{0x00, 0X00}), //TODO Decode Up Function Features
+	)
+
+	if i.Length != 6 {
+		t.Fatalf("Unexpected length. want 6, have %d", i.Length)
+	}
+
+	ba := []byte{0x00, 0x2B, 0x00, 0x06, 0x00, 0x00}
+
+	if b, _ := i.Serialize(); !bytes.Equal(b, ba) {
+		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, b)
+	}
+
+}
+
+func TestIECPFunctionFeatures(t *testing.T) {
+
+	i := NewInformationElement(
+		IECPFunctionFeatures,         //IEcode
+		0,                            //EntrepriseID
+		dt.OctetString([]byte{0x00}), //TODO Decode Cp Function Features
+	)
+
+	if i.Length != 5 {
+		t.Fatalf("Unexpected length. want 5, have %d", i.Length)
+	}
+
+	ba := []byte{0x00, 0x59, 0x00, 0x05, 0x00}
+
+	if b, _ := i.Serialize(); !bytes.Equal(b, ba) {
+		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, b)
+	}
 
 }
