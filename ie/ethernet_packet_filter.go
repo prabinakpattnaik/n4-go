525d6c6730e876a0ad3fdb8a9a8f225326622854
sothy5 <sothy.e98@gmail.com>
2019-10-09 22:16:31 +0200

    three IEs are added

---
+++ ie/ethernet_packet_filter.go
@@ -0,0 +1,12 @@
+package ie
+
+type EthernetPacketFilter struct {
+      EthernetFilterID          *InformationElement,
+      EthernetFilterProperties  *InformationElement,
+      MACAddress                *InformationElement,
+      Ethertype                 *InformationElement,
+      CTAG                      *InformationElement,
+      STAG                      *InformationElement,
+      SDFFilter                 *InformationElement
+
+}
---
+++ ie/pdi.go
@@ -0,0 +1,18 @@
+package ie
+
+type PDI struct {
+
+	LocalFTEID                    *InformationElement,
+	NetworkInstance               *InformationElement,
+	UEIPAddress                   *InformationElement,
+	TrafficEndpointID             *InformationElement,
+	SDFFilter                     *InformationElement,
+	ApplicationID                 *InformationElement,
+	EthernetPDUSessionInformation *InformationElement,
+	EthernetPacketFilter          *InformationElement,
+	QFI                           *InformationElement,
+	FramedRoute                   *InformationElement,
+	FramedRouting                 *InformationElement,
+	FramedIPv6Route               *InformationElement,
+
+}
