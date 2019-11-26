7309294a07a72292f78518d42ccec78ae533cd57
sothy5 <sothy.e98@gmail.com>
2019-11-26 14:17:31 +0100

    pfcpsession modification request with unit test is added

---
+++ msg/pfcp_session_modification_request.go
@@ -0,0 +1,197 @@
+package msg
+
+import (
+	"bitbucket.org/sothy5/n4-go/ie"
+)
+
+//PFCPSessionModificationRequest
+type PFCPSessionModificationRequest struct {
+	Header                   *PFCPHeader
+	CPFSEID                  *ie.InformationElement
+	RemovePDR                *ie.InformationElement
+	RemoveFAR                *ie.InformationElement
+	RemoveURR                *ie.InformationElement
+	RemoveQER                *ie.InformationElement
+	RemoveBAR                *ie.InformationElement
+	RemoveTrafficEndpoint    *ie.InformationElement
+	CreatePDR                *ie.InformationElement
+	CreateFAR                *ie.InformationElement
+	CreateURR                *ie.InformationElement
+	CreateQER                *ie.InformationElement
+	CreateBAR                *ie.InformationElement
+	CreateTrafficEndpoint    *ie.InformationElement
+	UpdatePDR                *ie.InformationElement
+	UpdateFAR                *ie.InformationElement
+	UpdateURR                *ie.InformationElement
+	UpdateQER                *ie.InformationElement
+	UpdateBAR                *ie.InformationElement
+	UpdateTrafficEndpoint    *ie.InformationElement
+	PFCPSMReqFlags           *ie.InformationElement
+	QueryURR                 *ie.InformationElement
+	UserPlaneInactivityTimer *ie.InformationElement
+	QueryURRReference        *ie.InformationElement
+	TraceInformation         *ie.InformationElement
+}
+
+//NewPFCPSessionModificationRequest creates new PFCPSessionMondificationRequst
+func NewPFCPSessionModificationRequest(h *PFCPHeader, cpfseid, rPDR, rFAR, rURR, rQER, rBAR, rTE, cPDR, cFAR, cURR, cQER, cBAR, cTE, uPDR, uFAR, uURR, uQER, uBAR, uTE, pfcpSMReqFlags, qURR, userPIT, qURRReference, traceInformation *ie.InformationElement) PFCPSessionModificationRequest {
+
+	return PFCPSessionModificationRequest{
+
+		Header:                   h,
+		CPFSEID:                  cpfseid,
+		RemovePDR:                rPDR,
+		RemoveFAR:                rFAR,
+		RemoveURR:                rURR,
+		RemoveQER:                rQER,
+		RemoveBAR:                rBAR,
+		RemoveTrafficEndpoint:    rTE,
+		CreatePDR:                cPDR,
+		CreateFAR:                cFAR,
+		CreateURR:                cURR,
+		CreateQER:                cQER,
+		CreateBAR:                cBAR,
+		CreateTrafficEndpoint:    cTE,
+		UpdatePDR:                uPDR,
+		UpdateFAR:                uFAR,
+		UpdateURR:                uURR,
+		UpdateQER:                uQER,
+		UpdateBAR:                uBAR,
+		UpdateTrafficEndpoint:    uTE,
+		PFCPSMReqFlags:           pfcpSMReqFlags,
+		QueryURR:                 qURR,
+		UserPlaneInactivityTimer: userPIT,
+		QueryURRReference:        qURRReference,
+		TraceInformation:         traceInformation,
+	}
+
+}
+
+func (smr PFCPSessionModificationRequest) Serialize() ([]byte, error) {
+	pfcpend := uint16(PFCPBasicHeaderLength) + PFCPMessageSize
+	output := smr.Header.Serialize()
+	b := []byte{}
+	var err error
+	if smr.CPFSEID != nil {
+		b, err = smr.CPFSEID.Serialize()
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	if smr.RemovePDR != nil {
+		rpdr, err := smr.RemovePDR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rpdr...)
+	}
+
+	if smr.RemoveFAR != nil {
+		rfar, err := smr.RemoveFAR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rfar...)
+	}
+
+	if smr.RemoveURR != nil {
+		rurr, err := smr.RemoveURR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rurr...)
+	}
+
+	if smr.RemoveQER != nil {
+		rqer, err := smr.RemoveQER.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rqer...)
+	}
+
+	if smr.RemoveBAR != nil {
+		rbar, err := smr.RemoveBAR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rbar...)
+	}
+
+	if smr.RemoveTrafficEndpoint != nil {
+		rte, err := smr.RemoveTrafficEndpoint.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, rte...)
+	}
+
+	if smr.CreatePDR != nil {
+		cpdr, err := smr.CreatePDR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, cpdr...)
+	}
+
+	if smr.CreateFAR != nil {
+		cfar, err := smr.CreateFAR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, cfar...)
+	}
+
+	if smr.CreateURR != nil {
+		curr, err := smr.CreateURR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, curr...)
+	}
+
+	if smr.CreateQER != nil {
+		cqer, err := smr.CreateQER.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, cqer...)
+	}
+
+	if smr.CreateBAR != nil {
+		cbar, err := smr.CreateBAR.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, cbar...)
+	}
+
+	if smr.CreateTrafficEndpoint != nil {
+		cte, err := smr.CreateTrafficEndpoint.Serialize()
+		if err != nil {
+			return nil, err
+		}
+		b = append(b, cte...)
+	}
+
+	copy(output[pfcpend:], b)
+
+	//TODO: remaining from UpdateIE, .....
+
+	return output, nil
+
+}
+
+func (smr PFCPSessionModificationRequest) Len() uint16 {
+	return uint16(PFCPBasicHeaderLength) + smr.Header.MessageLength
+}
+
+func (smr PFCPSessionModificationRequest) Type() PFCPType {
+	return smr.Header.MessageType
+}
+
+func (smr PFCPSessionModificationRequest) GetHeader() *PFCPHeader {
+	return smr.Header
+}
---
+++ msg/pfcp_session_modification_request_test.go
@@ -0,0 +1,168 @@
+package msg
+
+import (
+	"bytes"
+	"net"
+	"testing"
+
+	"bitbucket.org/sothy5/n4-go/ie"
+	dt "github.com/fiorix/go-diameter/diam/datatype"
+)
+
+func TestPFCPSessionModificationRequest(t *testing.T) {
+	sei := uint64(400)
+	sn := uint32(200)
+
+	ruleID := []byte{0x00, 0x10}
+	pdrid := ie.NewInformationElement(
+		ie.IEPDRID,
+		0,
+		dt.OctetString(ruleID),
+	)
+
+	pre := uint32(10)
+	precedence := ie.NewInformationElement(
+		ie.IEPrecedence,
+		0,
+		dt.Unsigned32(pre),
+	)
+
+	farId := dt.Unsigned32(100)
+	farID := ie.NewInformationElement(
+		ie.IEFARID,
+		0,
+		farId,
+	)
+
+	sourceinterface := uint8(1)
+	si := ie.NewInformationElement(
+		ie.IESourceInterface,
+		0,
+		dt.OctetString([]byte{sourceinterface}),
+	)
+
+	//ueIPAddress, destinationIP address,
+	ip := net.ParseIP("192.0.2.1")
+	ueIPAddress := ie.NewUEIPAddress(false, true, true, false, ip, nil, 0)
+	bb, err := ueIPAddress.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+
+	ueIPAddressIE := ie.NewInformationElement(
+		ie.IEUEIPaddress,
+		0,
+		dt.OctetString(bb),
+	)
+
+	pdi := ie.NewPDI(&si, nil, nil, &ueIPAddressIE, nil, nil, nil, nil, nil, nil, nil, nil, nil)
+	pdiB, err := pdi.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing PDI struct %+v", err)
+
+	}
+
+	pdiIE := ie.NewInformationElement(
+		ie.IEPDI,
+		0,
+		dt.OctetString(pdiB),
+	)
+
+	createPDR := ie.NewCreatePDR(&pdrid, &precedence, &pdiIE, nil, &farID, nil, nil, nil)
+	b, err := createPDR.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing CreatePDR %+v", err)
+
+	}
+
+	createPDRIE := ie.NewInformationElement(
+		ie.IECreatePDR, //IEcode
+		0,              //EntrepriseID
+		dt.OctetString(b),
+	)
+	length := ie.IEBasicHeaderSize + createPDRIE.Len()
+
+	//CreateFAR
+
+	applyAction := dt.OctetString([]byte{0x02})
+	a := ie.NewInformationElement(
+		ie.IEApplyAction,
+		0,
+		applyAction,
+	)
+
+	destinationinterface := uint8(ie.Access)
+	d := ie.NewInformationElement(
+		ie.IEDestinationInterface,
+		0,
+		dt.OctetString([]byte{destinationinterface}),
+	)
+
+	ohcd := uint8(1)
+	teid := uint32(100)
+	ip4address := net.ParseIP("8.8.8.8")
+	ohc := ie.NewOuterHeaderCreation(ohcd, teid, ip4address, nil, 0)
+	b, err = ohc.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+
+	ohcIE := ie.NewInformationElement(
+		ie.IEOuterHeaderCreation,
+		0,
+		dt.OctetString(b),
+	)
+
+	fp := ie.NewForwardingParameters(&d, nil, nil, &ohcIE, nil, nil, nil, nil, nil)
+	bb, err = fp.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+
+	fpIE := ie.NewInformationElement(
+		ie.IEForwardingParameters,
+		0,
+		dt.OctetString(bb),
+	)
+
+	createFAR := ie.NewCreateFAR(&farID, &a, &fpIE, nil, nil)
+	bCreateFAR, err := createFAR.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+
+	}
+
+	createFARIE := ie.NewInformationElement(
+		ie.IECreateFAR, //IEcode
+		0,              //EntrepriseID
+		dt.OctetString(bCreateFAR),
+	)
+	length = length + ie.IEBasicHeaderSize + createFARIE.Len()
+	pfcpHeader := NewPFCPHeader(1, false, true, SessionModificationRequestType, length+12, sei, sn, 0)
+
+	smr := NewPFCPSessionModificationRequest(pfcpHeader, nil, nil, nil, nil, nil, nil, nil, &createPDRIE, &createFARIE, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil)
+
+	bb, err = smr.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+
+	ba := []byte{0x21, 0x34, 0x00, 0x5f,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x90, 0x00, 0x00, 0xc8, 0x00,
+		0x00, 0x01, 0x00, 0x28, //CreatePDR
+		0x00, 0x38, 0x00, 0x02, 0x00, 0x10, //IEPDRID
+		0x00, 0x1d, 0x00, 0x04, 0x00, 0x00, 0x00, 0x0a, //IEPrecedence
+		0x00, 0x02, 0x00, 0x0e, 0x00, 0x14, 0x00, 0x01, 0x01, 0x00, 0x5d, 0x00, 0x05, 0x06, 0xc0, 0x00, 0x02, 0x01, //IEPDI
+		0x00, 0x6c, 0x00, 0x04, 0x00, 0x00, 0x00, 0x64, //IEFARID
+		0x00, 0x03, 0x00, 0x23, //IECreateFAR
+		0x00, 0x6c, 0x00, 0x04, 0x00, 0x00, 0x00, 0x64, //IEFARID
+		0x00, 0x2c, 0x00, 0x01, 0x02, //IEApplyAction
+		0x00, 0x04, 0x00, 0x12, //IEForwarding Parameters
+		0x00, 0x2a, 0x00, 0x01, 0x00, //IEDestination Interface
+		0x00, 0x54, 0x00, 0x09, 0x01, 0x00, 0x00, 0x00, 0x64, 0x08, 0x08, 0x08, 0X08, //IEOuterHeaderCreation
+	}
+
+	if !bytes.Equal(bb, ba) {
+		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, bb)
+	}
+}
