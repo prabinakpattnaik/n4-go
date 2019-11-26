29b722ae4d8def1a57f757907d27cfa8d17f8ad5
sothy shan <sothy.e98@gmail.com>
2019-11-26 13:23:09 +0000

    Merged in SESSION_MODIFICATION_I (pull request #33)
    SESSION MODIFICATION I
    
    * Interface enum added
    
    * unit test added and correction
    
    * outer header creation struct added
    
    * unit test includes outer header creation
    
    * update unit test
    
    * remove a unneeded line
    
    * pfcpsession modification request with unit test is added

--- ie/forwarding_parameters_test.go
+++ ie/forwarding_parameters_test.go
@@ -2,6 +2,7 @@ package ie
 
 import (
 	"bytes"
+	"net"
 	"testing"
 
 	dt "github.com/fiorix/go-diameter/diam/datatype"
@@ -55,19 +56,33 @@ func TestIEForwardingParameters(t *testing.T) {
 
 func TestForwardingParameters(t *testing.T) {
 	destinationinterface := uint8(1)
-
 	d := NewInformationElement(
 		IEDestinationInterface,
 		0,
 		dt.OctetString([]byte{destinationinterface}),
 	)
 
-	fp := NewForwardingParameters(&d, nil, nil, nil, nil, nil, nil, nil, nil)
-	ba := []byte{0x00, 0x2a, 0x00, 0x01, 0x01}
+	ohcd := uint8(1)
+	teid := uint32(100)
+	ip4address := net.ParseIP("8.8.8.8")
+	ohc := NewOuterHeaderCreation(ohcd, teid, ip4address, nil, 0)
+	b, err := ohc.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+
+	ohcIE := NewInformationElement(
+		IEOuterHeaderCreation,
+		0,
+		dt.OctetString(b),
+	)
+
+	fp := NewForwardingParameters(&d, nil, nil, &ohcIE, nil, nil, nil, nil, nil)
+	ba := []byte{0x00, 0x2a, 0x00, 0x01, 0x01, 0x00, 0x54, 0x00, 0x09, 0x01, 0x00, 0x00, 0x00, 0x64, 0x08, 0x08, 0x08, 0x08}
+
 	bb, err := fp.Serialize()
 	if err != nil {
 		t.Fatalf("Error in serializing %+v", err)
-
 	}
 
 	if !bytes.Equal(bb, ba) {
