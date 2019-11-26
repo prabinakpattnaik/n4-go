7b71389bd0390c072d58b0e884c6f831c502319b
sothy5 <sothy.e98@gmail.com>
2019-11-26 10:48:46 +0100

    unit test added and correction

--- ie/outer_header_creation_test.go
+++ ie/outer_header_creation_test.go
@@ -2,14 +2,16 @@ package ie
 
 import (
 	"bytes"
-	dt "github.com/fiorix/go-diameter/diam/datatype"
+	"net"
 	"testing"
+
+	dt "github.com/fiorix/go-diameter/diam/datatype"
 )
 
-func TestNewOuterHeaderCreation(t *testing.T) {
+func TestIEOuterHeaderCreation(t *testing.T) {
 	// GTP-U/UDP/IPv4 (5/1)
 	//TEID
-	outerHeaderCreation := dt.OctetString([]byte{0x01, 0x00, 0x00, 0x00, 0xff})
+	outerHeaderCreation := dt.OctetString([]byte{0x01, 0x00, 0x00, 0x00, 0xff, 0x08, 0x08, 0x08, 0x8})
 
 	i := NewInformationElement(
 		IEOuterHeaderCreation,
@@ -26,12 +28,30 @@ func TestNewOuterHeaderCreation(t *testing.T) {
 		t.Fatalf("Error in serializing %+v", err)
 	}
 
-	ba := []byte{0x00, 0x54, 0x00, 0x5, 0x01, 0x00, 0x00, 0x00, 0xff}
+	ba := []byte{0x00, 0x54, 0x00, 0x09, 0x01, 0x00, 0x00, 0x00, 0xff, 0x08, 0x08, 0x08, 0x08}
 
 	if !bytes.Equal(bb, ba) {
 		t.Fatalf("outerHeaderCreation:unexpected value. want [%x}, have [%x]", ba, bb)
 	}
 
 	t.Log(i)
+
+}
+
+func TestNewOuterHeaderCreation(t *testing.T) {
+	ohcd := uint8(1)
+	teid := uint32(100)
+	ip4address := net.ParseIP("8.8.8.8")
+
+	ohc := NewOuterHeaderCreation(ohcd, teid, ip4address, nil, 0)
+	b, err := ohc.Serialize()
+	if err != nil {
+		t.Fatalf("Error in serializing %+v", err)
+	}
+	a := []byte{0x01, 0x00, 0x00, 0x00, 0x64, 0x08, 0x08, 0x08, 0x08}
+
+	if !bytes.Equal(b, a) {
+		t.Fatalf("outerHeaderCreation:unexpected value. want [%x}, have [%x]", a, b)
+	}
 
 }
