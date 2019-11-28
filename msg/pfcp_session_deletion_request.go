3d89a1f30b799686e5c1eccba8b9130c9211b909
sothy5 <sothy.e98@gmail.com>
2019-11-28 14:52:32 +0100

    adding pfcp session deletion request struct

---
+++ msg/pfcp_session_deletion_request.go
@@ -0,0 +1,33 @@
+package msg
+
+import (
+	"fmt"
+)
+
+//PFCPSessionDeletionRequest
+type PFCPSessionDeletionRequest struct {
+	Header *PFCPHeader
+}
+
+//NewPFCPSessionModificationRequest creates new PFCPSessionMondificationRequst
+func NewPFCPSessionDeletionRequest(h *PFCPHeader) PFCPSessionDeletionRequest {
+	return PFCPSessionDeletionRequest{
+		Header: h,
+	}
+}
+func (sdr PFCPSessionDeletionRequest) Serialize() ([]byte, error) {
+	fmt.Printf("[%x]\n", sdr.Header.Serialize())
+	return sdr.Header.Serialize(), nil
+}
+
+func (sdr PFCPSessionDeletionRequest) Len() uint16 {
+	return uint16(PFCPBasicHeaderLength) + sdr.Header.MessageLength
+}
+
+func (sdr PFCPSessionDeletionRequest) Type() PFCPType {
+	return sdr.Header.MessageType
+}
+
+func (sdr PFCPSessionDeletionRequest) GetHeader() *PFCPHeader {
+	return sdr.Header
+}
