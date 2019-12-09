f200fa044e014f4ef10ecd6aa349de7719a818e3
sothy shan <sothy.e98@gmail.com>
2019-12-09 10:27:47 +0000

    Merged in CLIENTSERVER9 (pull request #42)
    CLIENTSERVER9
    
    * correction on fteid
    
    * correction on session management test
    
    * client and association corrections

--- ie/fteid_test.go
+++ ie/fteid_test.go
@@ -15,13 +15,12 @@ func TestFTEID(t *testing.T) {
 	ba := []byte{0x01, 0x00, 0x00, 0x00, 0xFF, 0xC0, 0xA8, 0x1, 0x65}
 
 	bb, err := fteid.Serialize()
-
 	if err != nil {
 		t.Fatalf("Error in serializing %+v", err)
 
 	}
 	if !bytes.Equal(ba, ba) {
-		t.Fatalf("unexpected value. want [%x}, have [%x]", ba, bb)
+		t.Fatalf("unexpected value. want [%x], have [%x]", ba, bb)
 	}
 
 }
